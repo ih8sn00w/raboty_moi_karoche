80d4c6138ed2e316e4ffe84531f2a6dc03c3e487
ih8sn00w <toni.kuchmistyy@mail.ru>
2023-11-24 01:02:11 +0300

    game adaptation on prints ("Ну Погоди!")

--- labs/lab_10.py
+++ labs/lab_10.py
@@ -5,122 +5,141 @@
 """
 
 
-def f(digit):
-    return digit ** 2 + 3 * digit - 1
+def f(x):
+    return x ** 2 + 3 * x + 5
 
 
-def F(digit):
-    return digit ** 3 / 3 + 3 * digit ** 2 / 2 - digit
+def F(x):
+    return 1 / 3 * x ** 3 + 3 / 2 * x ** 2 + 5 * x
 
 
-start_pose = float(input('Введите начало отрезка: '))
-end_pose = float(input('Введите конец отрезка: '))
-count_1 = int(input('Введите колличество участков разбиения 1: '))
-count_2 = int(input('Введите колличество участков разбиения 2: '))
+def isint(n):
+    line = '-1234567890'
+    fl = True
+    for elem in str(n):
+        if elem not in line:
+            fl = False
+    return fl
+
+
+def function(prompt):
+    var = input(prompt)
+    while not isint(var):
+        var = input()
+    var = int(var)
+    return var
+
 
 # 1 способ 1 колличество разбиений
 
-l1 = 0
-for i in range(count_1):
-    x = f(start_pose + ((end_pose - start_pose) / count_1) * i)
-    dx = ((end_pose - start_pose) / count_1)
-    l1 += x * dx
-print(l1)
 
-# 1 способ 2 колличество разбиений
+def dunc(end_pose, start_pose, count_1):
+    q = 0
+    for i in range(count_1):
+        x = f(start_pose + ((end_pose - start_pose) / count_1) * i)
+        dx = ((end_pose - start_pose) / count_1)
+        q += x * dx
+    return q
 
-l2 = 0
-for i in range(count_2):
-    a = f(start_pose + ((end_pose - start_pose) / count_2) * i)
-    l2 += a * ((end_pose - start_pose) / count_2)
-print(l2)
 
 # 2 способ 1 колличество разбиений
 
-l3 = 0
-x2 = start_pose
-for i in range(1, count_1 + 1):
-    x1 = x2
-    x2 = start_pose + ((end_pose - start_pose) / count_1) * i
-    dx = ((end_pose - start_pose) / count_1)
-    l3 += dx * (f(x1) + 3 * f((2 * x1 + x2) / 3) + 3 * f((x1 + 2 * x2) / 3) + f(x2)) / 8
-print(l3)
 
-# 2 способ 2 колличество разбиений
+def pykikakikakashki(start_pose, end_pose, count_1):
+    l3 = 0
+    x2 = start_pose
+    for i in range(1, count_1 + 1):
+        x1 = x2
+        x2 = start_pose + ((end_pose - start_pose) / count_1) * i
+        dx = ((end_pose - start_pose) / count_1)
+        l3 += dx * (f(x1) + 3 * f((2 * x1 + x2) / 3) + 3 * f((x1 + 2 * x2) / 3) + f(x2)) / 8
+    return l3
 
-l4 = 0
-x2 = start_pose
-for i in range(1, count_2 + 1):
-    x1 = x2
-    x2 = start_pose + ((end_pose - start_pose) / count_2) * i
-    dx = ((end_pose - start_pose) / count_2)
-    l4 += dx * (f(x1) + 3 * f((2 * x1 + x2) / 3) + 3 * f((x1 + 2 * x2) / 3) + f(x2)) / 8
-print(l4)
 
-# Вывод таблицы
+def main():
+    start_pose = function('Введите начало отрезка: ')
+    end_pose = function('Введите конец отрезка: ')
+    count_1 = function('Введитe количество участков разбиения 1: ')
+    count_2 = function('Введитe количество участков разбиения 2: ')
 
-print('-' * 69)
-print('|' + '-' * 28, '|' + f'{count_1:^18.7g}' + '|' + f'{count_2:^18.7g}' + '|')
-print('-' * 69)
-print('|' + ' метод левых прямоугольников ' + '|' + f'{l1:^18.7g}' + '|' + f'{l2:^18.7g}' + '|')
-print('-' * 69)
-print('|' + '          метод 3/8          ' + '|' + f'{l3:^18.7g}' + '|' + f'{l4:^18.7g}' + '|')
-print('-' * 69)
+    l1 = dunc(end_pose, start_pose, count_1)
+    l2 = dunc(end_pose, start_pose, count_2)
+    print(l1)
+    print(l2)
 
-# вычисление погршностей измерений
-print("\n\n")
+    l3 = pykikakikakashki(start_pose, end_pose, count_1)
+    l4 = pykikakikakashki(start_pose, end_pose, count_2)
 
-print("-" * 85)
+    # Вывод таблицы
 
-print(f"|{'l1':^20}|{'l2':^20}|{'l3':^20}|{'l4':^20}|")
+    print('-' * 69)
+    print('|' + '-' * 28, '|' + f'{count_1:^18.7g}' + '|' + f'{count_2:^18.7g}' + '|')
+    print('-' * 69)
+    print('|' + ' метод левых прямоугольников ' + '|' + f'{l1:^18.7g}' + '|' + f'{l2:^18.7g}' + '|')
+    print('-' * 69)
+    print('|' + '          метод 3/8          ' + '|' + f'{l3:^18.7g}' + '|' + f'{l4:^18.7g}' + '|')
+    print('-' * 69)
 
-print("-" * 85)
+    # вычисление погршностей измерений
+    print("\n\n")
 
-a = F(start_pose)
-b = F(end_pose)
-q = b - a
-dl1 = l1 - q
-dl2 = l2 - q
-dl3 = l3 - q
-dl4 = l4 - q
+    print("-" * 85)
 
-print(f'|{dl1:^20.5g}|{dl2:^20.5g}|{dl3:^20.5g}|{dl4:^20.5g}|')
-print("-" * 85)
+    print(f"|{'l1':^20}|{'l2':^20}|{'l3':^20}|{'l4':^20}|")
 
-dl1 = abs(dl1 / q * 100)
-dl2 = abs(dl2 / q * 100)
-dl3 = abs(dl3 / q * 100)
-dl4 = abs(dl4 / q * 100)
+    print("-" * 85)
 
-print(f'|{dl1:^20.5g}|{dl2:^20.5g}|{dl3:^20.5g}|{dl4:^20.5g}|')
-print("-" * 85)
+    a = F(start_pose)
+    b = F(end_pose)
+    q = b - a
+    dl1 = l1 - q
+    dl2 = l2 - q
+    dl3 = l3 - q
+    dl4 = l4 - q
 
-# повышение точности
+    print(f'|{abs(dl1):^20.5g}|{abs(dl2):^20.5g}|{abs(dl3):^20.5g}|{abs(dl4):^20.5g}|')
+    print("-" * 85)
 
-eps = 1e-4
-if dl1 >= dl3 or dl2 > dl4:
-    N = count_1
-    while True:
-        l = 0
-        N = 2 * N
-        for i in range(2 * N):
-            x = f(start_pose + ((end_pose - start_pose) / N) * i)
-            dx = ((end_pose - start_pose) / N)
-            l += x * dx
-        if abs(l1 - l) < eps:
-            print(l, N)
-            break
-        l1 = l
-else:
-    N = count_2
-    while True:
-        l = 0
-        N = 2 * N
-        for i in range(2 * N):
-            x = f(start_pose + ((end_pose - start_pose) / N) * i)
-            dx = ((end_pose - start_pose) / N)
-            l += x * dx
-        if abs(l3 - l) < eps:
-            print(l, N)
-            break
-        l3 = l
+    dl1 = abs(dl1 / q * 100)
+    dl2 = abs(dl2 / q * 100)
+    dl3 = abs(dl3 / q * 100)
+    dl4 = abs(dl4 / q * 100)
+
+    print(f'|{dl1:^20.5g}|{dl2:^20.5g}|{dl3:^20.5g}|{dl4:^20.5g}|')
+    print("-" * 85)
+
+    # повышение точности
+
+    eps = float(input('Введите погрешность '))
+
+    if dl1 >= dl3 or dl2 > dl4:
+        N = count_1
+        while True:
+            l = 0
+            N = 2 * N
+            for i in range(2 * N):
+                x = f(start_pose + ((end_pose - start_pose) / N) * i)
+                dx = ((end_pose - start_pose) / N)
+                l += x * dx
+            print(l1 - l)
+            if abs(l1 - l) < eps:
+                print(N)
+                break
+            l1 = l
+    else:
+        N = count_2
+        while True:
+            l = 0
+            N = 2 * N
+            for i in range(2 * N):
+                x = f(start_pose + ((end_pose - start_pose) / N) * i)
+                dx = ((end_pose - start_pose) / N)
+                l += x * dx
+            print(l3 - l)
+            if abs(l3 - l) < eps:
+                print(N)
+                break
+            l3 = l
+
+if __name__ == '__main__':
+    main()
---
+++ tik-toe.py
@@ -0,0 +1,75 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("TIC-TAC-TOE")
+root.geometry("500x550")  # Немного увеличил высоту, чтобы вместить метку сообщения
+
+buttons = [[None for _ in range(3)] for _ in range(3)]  # 2D список для хранения кнопок
+
+for c in range(3):
+    root.columnconfigure(index=c, weight=1)
+for r in range(4):  # Увеличил количество строк для метки сообщения
+    root.rowconfigure(index=r, weight=1)
+
+game_over = False
+
+
+def click(row, col):
+    global k, game_over
+    if not game_over and buttons[row][col]["text"] == "":
+        if k % 2 == 0:
+            buttons[row][col]["text"] = "O"
+        else:
+            buttons[row][col]["text"] = "X"
+        k += 1
+        check_winner()
+
+
+def check_winner():
+    global game_over
+    # Проверка строк, столбцов и диагоналей на наличие выигрышной комбинации
+    for i in range(3):
+        if buttons[i][0]["text"] == buttons[i][1]["text"] == buttons[i][2]["text"] != "":
+            highlight_winner(buttons[i][0], buttons[i][1], buttons[i][2])
+            return
+        if buttons[0][i]["text"] == buttons[1][i]["text"] == buttons[2][i]["text"] != "":
+            highlight_winner(buttons[0][i], buttons[1][i], buttons[2][i])
+            return
+    if buttons[0][0]["text"] == buttons[1][1]["text"] == buttons[2][2]["text"] != "":
+        highlight_winner(buttons[0][0], buttons[1][1], buttons[2][2])
+        return
+    if buttons[0][2]["text"] == buttons[1][1]["text"] == buttons[2][0]["text"] != "":
+        highlight_winner(buttons[0][2], buttons[1][1], buttons[2][0])
+        return
+    # Если нет победителя и все кнопки заполнены, это ничья
+    if all(button["text"] != "" for row in buttons for button in row):
+        game_over = True
+        message_label.config(text="It's a tie!")  # Обновление метки сообщения
+
+
+def highlight_winner(*winning_buttons):
+    global game_over
+    winner_label = Label(root, text="Winner!", font=("Arial", 16, "bold"))
+    winner_label.grid(row=3, columnspan=3)  # Размещение метки над сеткой
+    for button in winning_buttons:
+        button.configure(style="Winner.TButton")
+    game_over = True
+    message_label.config(text="Winner!")  # Отображение сообщения о победе
+
+
+k = 1
+
+style = ttk.Style()
+style.configure("Winner.TButton", foreground="black")  # Определение стиля для кнопок-победителей
+
+for r in range(3):
+    for c in range(3):
+        btn = ttk.Button(root, text="", command=lambda row=r, col=c: click(row, col), style="Winner.TButton")
+        btn.grid(row=r, column=c, ipadx=100, ipady=100, padx=4, pady=4)
+        buttons[r][c] = btn  # Хранение кнопки в 2D списке
+
+message_label = Label(root, text="", font=("Arial", 16))
+message_label.grid(row=4, columnspan=3)  # Добавление метки для сообщений
+
+root.mainloop()
